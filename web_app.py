8cc78beaf6f483ed86b70016712e38f20bba50cc
Elvis KONJOH <21037718+loctet@users.noreply.github.com>
2025-06-07 18:55:33 +0200

    deploy

--- web_app.py
+++ web_app.py
@@ -94,4 +94,5 @@ def process():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    #app.run(debug=True, port=5000) 
+    app.run(host="0.0.0.0", debug=True, port=5000)
